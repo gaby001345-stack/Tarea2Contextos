3a42caf1119f91e59549f777f67c90512e60b9e2
gaby001345-stack <gaby001345@gmail.com>
2026-06-02 21:53:46 -0600

    Se eliminó el contenido de prueba en App.tsx y se integró la estructura de navegación con ThemeProvider y AuthProvider.

--- MiAppContext/App.tsx
+++ MiAppContext/App.tsx
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { AuthProvider } from "./src/contexts/AuthContext";
+import { ThemeProvider } from "./src/contexts/ThemeContext";
+import StackNavigator from "./src/navigation/StackNavigator";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider>
+      <AuthProvider>
+        <NavigationContainer>
+          <StackNavigator />
+        </NavigationContainer>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
